a2abba9872e3830c0b7928537cb9654fbc51642a
zhihaofans <zhihaofans@hotmail.com>
2024-08-15 23:52:38 +0800

    Initial Commit

---
+++ Hello Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello Swift
+//
+//  Created by zzh on 2024/8/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello Swift/Hello_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftApp.swift
+//  Hello Swift
+//
+//  Created by zzh on 2024/8/15.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
