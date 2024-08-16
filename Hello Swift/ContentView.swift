340f0a1675879b78a64ffb85bfd232552e61f685
zhihaofans <zhihaofans@hotmail.com>
2024-08-16 09:05:43 +0800

    update ContentView for ios

--- Hello Swift/ContentView.swift
+++ Hello Swift/ContentView.swift
@@ -6,8 +6,73 @@
 //
 
 import SwiftUI
+import SwiftUtils
 
 struct ContentView: View {
+    var body: some View {
+        #if os(iOS)
+        iosMainView()
+        #else
+        EmptyView()
+        #endif
+    }
+}
+
+struct iosMainView: View {
+    @State private var selectedTab = 0
+    var body: some View {
+        switch selectedTab {
+        default:
+            NavigationView {
+                List {
+                    NavigationLink("签到", destination: EmptyView())
+                    NavigationLink("稍后再看", destination: EmptyView())
+                    NavigationLink("历史记录", destination: EmptyView())
+                    // NavigationLink("动态", destination: HistoryView())
+                    NavigationLink("工具", destination: EmptyView())
+                }
+                .navigationTitle(AppUtil().getAppName() /* "哔了个哩" */ )
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: EmptyView()) {
+                            // TODO: 这里跳转到个人页面或登录界面
+                            Image(systemName: "person")
+                        }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        NavigationLink(destination: EmptyView()) {
+                            Image(systemName: "gear")
+                        }
+                    }
+                }
+            }
+        }
+        TabView(selection: $selectedTab) {
+            Text("")
+                .tabItem {
+                    Label("主页", systemImage: "house")
+                }
+                .tag(0)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("动态", systemImage: "fanblades")
+                }
+                .tag(1)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("更多", systemImage: "ellipsis")
+                }
+                .tag(2)
+        }
+        .frame(maxHeight: 50) // 限制最大高度
+    }
+}
+
+struct EmptyView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
