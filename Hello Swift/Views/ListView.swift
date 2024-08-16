2a397397074bb404158ad82925a6b2a58c863383
zhihaofans <zhihaofans@hotmail.com>
2024-08-16 11:42:18 +0800

    update ListView

--- Hello Swift/Views/ListView.swift
+++ Hello Swift/Views/ListView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ListView: View {
+    private let viewTitle = "列表"
+    @State var listView = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                NavigationLink("列表", destination: ListView())
+            }
+            .navigationTitle(viewTitle)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: EmptyView()) {
+                        // TODO: 这里跳转到个人页面或登录界面
+                        Image(systemName: "person")
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: EmptyView()) {
+                        Image(systemName: "gear")
+                    }
+                }
+            }
+        }
     }
 }
-
+class ListItemData{
+    var title: String
+    var view: View
+    init(title: String,view: View){
+        self.title = title
+        self.view = view
+    }
+}
 #Preview {
     ListView()
 }
