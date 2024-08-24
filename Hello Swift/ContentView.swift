193958fe404d4244676defa50589072ceb462db4
zhihaofans <zhihaofans@hotmail.com>
2024-08-24 08:36:36 +0800

    update MainView

--- Hello Swift/ContentView.swift
+++ Hello Swift/ContentView.swift
@@ -10,7 +10,45 @@ import SwiftUtils
 
 struct ContentView: View {
     var body: some View {
-        MainView()
+        #if os(iOS)
+        ContentIOSView()
+        #else
+        Text("Hello Swift")
+        #endif
+    }
+}
+
+struct ContentIOSView: View {
+    @State private var selectedTab = 0
+    var body: some View {
+        switch selectedTab {
+        case 2:
+            SettingView()
+        default:
+            MainView()
+        }
+        TabView(selection: $selectedTab) {
+            Text("")
+                .tabItem {
+                    Label("主页", systemImage: "house")
+                }
+                .tag(0)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("动态", systemImage: "fanblades")
+                }
+                .tag(1)
+
+            Text("")
+                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
+                .tabItem {
+                    Label("更多", systemImage: "ellipsis")
+                }
+                .tag(2)
+        }
+        .frame(maxHeight: 50) // 限制最大高度
     }
 }
 
--- Hello Swift/Views/MainView.swift
+++ Hello Swift/Views/MainView.swift
@@ -21,57 +21,32 @@ struct MainView: View {
 struct iosMainView: View {
     @State private var selectedTab = 0
     var body: some View {
-        switch selectedTab {
-        default:
-            NavigationView {
-                List {
-                    Section("界面UI") {
-                        NavigationLink("列表List", destination: ListView())
-                        NavigationLink("弹窗Alert", destination: AlertView())
-                        NavigationLink("图片Image", destination: ImageView())
-                    }
-                    Section("功能API") {
-                        Text("学习中")
+        NavigationView {
+            List {
+                Section("界面UI") {
+                    NavigationLink("列表List", destination: ListView())
+                    NavigationLink("弹窗Alert", destination: AlertView())
+                    NavigationLink("图片Image", destination: ImageView())
+                }
+                Section("功能API") {
+                    Text("学习中")
+                }
+            }
+            .navigationTitle(AppUtil().getAppName())
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: EmptyView()) {
+                        // TODO: 这里跳转到个人页面或登录界面
+                        Image(systemName: "person")
                     }
                 }
-                .navigationTitle(AppUtil().getAppName())
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: EmptyView()) {
-                            // TODO: 这里跳转到个人页面或登录界面
-                            Image(systemName: "person")
-                        }
-                    }
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        NavigationLink(destination: SettingView()) {
-                            Image(systemName: "gear")
-                        }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: SettingView()) {
+                        Image(systemName: "gear")
                     }
                 }
             }
         }
-        TabView(selection: $selectedTab) {
-            Text("")
-                .tabItem {
-                    Label("主页", systemImage: "house")
-                }
-                .tag(0)
-
-            Text("")
-                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
-                .tabItem {
-                    Label("动态", systemImage: "fanblades")
-                }
-                .tag(1)
-
-            Text("")
-                .fixedSize(horizontal: false, vertical: true) // 纵向固定大小
-                .tabItem {
-                    Label("更多", systemImage: "ellipsis")
-                }
-                .tag(2)
-        }
-        .frame(maxHeight: 50) // 限制最大高度
     }
 }
 
